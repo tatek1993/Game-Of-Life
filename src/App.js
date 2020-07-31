b2702c4ab130befa8c67a6250b0c727f94efa002
Tate Keller <tatek1993@gmail.com>
2020-07-30 20:44:07 -0500

    Game running, start and stop implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import './App.css';
 import Grid from './components/grid';
 
 
+let array1 = CreateGrid(50, 50);
+let array2 = CreateGrid(50, 50);
+
+function CreateGrid(col, row) {
+  let arr = Array(col);
+  // For each column in arr we are inserting an array of equal size
+  // This creates a 2d array which we use to make a grid
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Array(row);
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = false;
+    }
+  }
+  return arr;
+}
+
 function App() {
 
   // For each cell in the current generation's grid:
@@ -12,25 +28,16 @@ function App() {
   // ----Swap current and next grids
   // ----Repeat until simulation stopped
 
-  const [arr, setArr] = useState(CreateGrid(50, 50));
+  const [arr, setArr] = useState(array1);
 
-  function CreateGrid(col, row) {
-    let arr = Array(col);
-    // For each column in arr we are inserting an array of equal size
-    // This creates a 2d array which we use to make a grid
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] = Array(row);
-      for (let j = 0; j < arr[i].length; j++) {
-        arr[i][j] = false;
-      }
-    }
-    return arr;
-  }
 
-  function Generation() {
-    // neighbor logic
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr[i].length; j++) {
+
+
+
+  function Generation(current, next) {
+    // NEIGHBOR LOGIC
+    for (let i = 0; i < current.length; i++) {
+      for (let j = 0; j < current[i].length; j++) {
         const neighbors = [
           { x: i - 1, y: j + 1 },
           { x: i, y: j + 1 },
@@ -42,14 +49,21 @@ function App() {
           { x: i + 1, y: j - 1 },
         ];
 
-        let currentCell = arr[i][j];
+        let currentCell = current[i][j];
         let liveNeighbors = 0;
-        let futureCell = false;
+        let futureCell = next[i][j];
 
         // for each cell in neighbors, if the cell is true, increment liveNeighbors
         neighbors.forEach((cell) => {
-          if (arr[cell.x % 50][cell.y % 50] == true) {
+          if (cell.x < 0) {
+            cell.x += 50;
+          }
+          if (cell.y < 0) {
+            cell.y += 50;
+          }
+          if (current[cell.x % 49][cell.y % 49] == true) {
             liveNeighbors++;
+
           }
         })
 
@@ -74,15 +88,45 @@ function App() {
             futureCell = false;
           }
         }
+        next[i][j] = futureCell;
       }
     }
 
-    // wrap grid
-    // determine generation
+
+  }
+
+  const [gen, setGen] = useState({ count: 0 });
+
+
+  const [intr, setIntr] = useState();
+
+  function onPlay(event) {
+    array1 = arr;
+    setIntr(setInterval(() => {
+      console.log("gen", gen);
+      if (gen.count % 2 === 0) {
+        Generation(array1, array2);
+        setArr(array2);
+      } else {
+        Generation(array2, array1);
+        setArr(array1);
+      }
+      gen.count++;
+      setGen(gen);
+
+    }, 100));
+  }
+
+  function onStop(event) {
+    clearInterval(intr);
   }
 
   return (
-    <Grid arr={arr} setArr={setArr} />
+    <>
+      <Grid arr={arr} setArr={setArr} />
+      <button onClick={onPlay}> Start </button>
+      <button onClick={onStop}> Stop </button>
+    </>
   );
 }
 
