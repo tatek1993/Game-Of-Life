fbd93195b6ce7765c5b1fd794ff78587920a9c1b
Tate Keller <tatek1993@gmail.com>
2020-09-22 17:39:47 -0500

    removing sideways scroll bar, adjusting padding and margin on css elements

--- src/App.js
+++ src/App.js
@@ -191,6 +191,7 @@ function App() {
 
   return (
     <>
+      <a href="https://github.com/tatek1993/Game-Of-Life"><img loading="lazy" width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" className="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
       <h1>Conway's Game Of Life</h1>
       <div className="container">
         <div className="game">
